1849e8fc9d5a81e1581b48f9cd6361ed19126ee2
Tony Ivanov <telamohn@gmail.com>
2020-04-08 23:06:16 +0200

    Refactored imports for treeshaking

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-const { PollMessage, PollStatement, IdentityMessage, VoteMsg } = require('./messages.js')
+const { PollMessage, PollStatement, IdentityMessage, VoteMsg } = require('./messages')
 const PicoFeed = require('picofeed')
-/* TODO: Rollup
+
+/* eslint-disable camelcase */
 const {
   crypto_kdf_CONTEXTBYTES,
-  crypto_kdf_KEYBYTES,
+  // crypto_kdf_KEYBYTES,
   crypto_kdf_derive_from_key,
-  crypto_kdf_keygen,
+  // crypto_kdf_keygen,
 
   crypto_sign_PUBLICKEYBYTES,
   crypto_sign_SECRETKEYBYTES,
   crypto_sign_SEEDBYTES,
   crypto_sign_BYTES,
   crypto_sign_seed_keypair,
+  crypto_sign_keypair,
   crypto_sign,
   crypto_sign_open,
   crypto_sign_detached,
@@ -23,6 +25,7 @@ const {
   crypto_box_SECRETKEYBYTES,
   crypto_box_PUBLICKEYBYTES,
   crypto_box_SEEDBYTES,
+  crypto_box_keypair,
   crypto_box_seed_keypair,
   crypto_box_easy,
   crypto_box_open_easy,
@@ -40,17 +43,17 @@ const {
   crypto_pwhash,
 
   randombytes_buf
-} = require('sodium-universal') */
-const sod = require('sodium-universal')
+} = require('sodium-universal')
+/* eslint-enable camelcase */
 
 const Util = {
   deriveSubkey (master, n, context = '__undef__', id = 0) {
     if (!Buffer.isBuffer(context)) context = Buffer.from(context)
     const sub = Buffer.alloc(n)
-    sod.crypto_kdf_derive_from_key(
+    crypto_kdf_derive_from_key(
       sub,
       id,
-      context.subarray(0, sod.crypto_kdf_CONTEXTBYTES),
+      context.subarray(0, crypto_kdf_CONTEXTBYTES),
       master
     )
     return sub
@@ -58,14 +61,14 @@ const Util = {
 
   deriveSignPair (master, ctx = 'IDENTITY', id = 0) {
     // crypto_sign_keypair  (crypto_sign_ed25519 this is what hypercore uses)
-    const sec = Buffer.alloc(sod.crypto_sign_SECRETKEYBYTES) // U64
-    const pub = Buffer.alloc(sod.crypto_sign_PUBLICKEYBYTES) // U32
-    sod.crypto_sign_seed_keypair(
+    const sec = Buffer.alloc(crypto_sign_SECRETKEYBYTES) // U64
+    const pub = Buffer.alloc(crypto_sign_PUBLICKEYBYTES) // U32
+    crypto_sign_seed_keypair(
       pub,
       sec,
       Util.deriveSubkey(
         master,
-        sod.crypto_sign_SEEDBYTES,
+        crypto_sign_SEEDBYTES,
         ctx,
         id
       )
@@ -74,14 +77,14 @@ const Util = {
   },
 
   deriveBoxPair (master, ctx = 'BOX00000', id = 0) {
-    const sec = Buffer.alloc(sod.crypto_box_SECRETKEYBYTES) // U64
-    const pub = Buffer.alloc(sod.crypto_box_PUBLICKEYBYTES) // U32
-    sod.crypto_box_seed_keypair(
+    const sec = Buffer.alloc(crypto_box_SECRETKEYBYTES) // U64
+    const pub = Buffer.alloc(crypto_box_PUBLICKEYBYTES) // U32
+    crypto_box_seed_keypair(
       pub,
       sec,
       Util.deriveSubkey(
         master,
-        sod.crypto_box_SEEDBYTES || 32, // TODO: report missing const in browser.
+        crypto_box_SEEDBYTES || 32, // TODO: report missing const in browser.
         ctx,
         id
       )
@@ -96,27 +99,27 @@ const Util = {
     if (!Buffer.isBuffer(data)) data = Buffer.from(data, 'utf-8')
 
     const o = Buffer.allocUnsafe(data.length +
-      sod.crypto_secretbox_NONCEBYTES +
-      sod.crypto_secretbox_MACBYTES)
+      crypto_secretbox_NONCEBYTES +
+      crypto_secretbox_MACBYTES)
 
-    const nonce = o.subarray(0, sod.crypto_secretbox_NONCEBYTES)
-    sod.randombytes_buf(nonce)
+    const nonce = o.subarray(0, crypto_secretbox_NONCEBYTES)
+    randombytes_buf(nonce)
 
-    sod.crypto_secretbox_easy(
-      o.subarray(sod.crypto_secretbox_NONCEBYTES),
+    crypto_secretbox_easy(
+      o.subarray(crypto_secretbox_NONCEBYTES),
       data, nonce, encryptionKey)
     return o
   },
 
   decrypt (buffer, encryptionKey, decode) {
     const message = Buffer.allocUnsafe(buffer.length -
-      sod.crypto_secretbox_MACBYTES -
-      sod.crypto_secretbox_NONCEBYTES)
+      crypto_secretbox_MACBYTES -
+      crypto_secretbox_NONCEBYTES)
 
-    const nonce = buffer.subarray(0, sod.crypto_secretbox_NONCEBYTES)
-    const success = sod.crypto_secretbox_open_easy(
+    const nonce = buffer.subarray(0, crypto_secretbox_NONCEBYTES)
+    const success = crypto_secretbox_open_easy(
       message,
-      buffer.subarray(sod.crypto_secretbox_NONCEBYTES),
+      buffer.subarray(crypto_secretbox_NONCEBYTES),
       nonce,
       encryptionKey)
 
@@ -144,50 +147,50 @@ const Util = {
   },
 
   sign (m, sk) {
-    const msgSig = Buffer.allocUnsafe(sod.crypto_sign_BYTES + m.length)
-    sod.crypto_sign(msgSig, m, sk)
+    const msgSig = Buffer.allocUnsafe(crypto_sign_BYTES + m.length)
+    crypto_sign(msgSig, m, sk)
     return msgSig
   },
 
   signOpen (sm, pk) {
-    const m = Buffer.allocUnsafe(sm.length - sod.crypto_sign_BYTES)
-    if (!sod.crypto_sign_open(m, sm, pk)) throw new Error('Message failed signature authentication')
+    const m = Buffer.allocUnsafe(sm.length - crypto_sign_BYTES)
+    if (!crypto_sign_open(m, sm, pk)) throw new Error('Message failed signature authentication')
     return m
   },
 
   signDetached (message, secretKey) {
-    const signature = Buffer.allocUnsafe(sod.crypto_sign_BYTES)
-    sod.crypto_sign_detached(signature, message, secretKey)
+    const signature = Buffer.allocUnsafe(crypto_sign_BYTES)
+    crypto_sign_detached(signature, message, secretKey)
     return signature
   },
 
   verifyDetached (message, signature, publicKey) {
-    return sod.crypto_sign_verify_detached(signature, message, publicKey)
+    return crypto_sign_verify_detached(signature, message, publicKey)
   },
 
   puzzleEncrypt (data, difficulty = 1) {
     if (difficulty > 3) throw new Error('Please mind the environment')
     const cipher = Buffer.allocUnsafe(data.length +
-      sod.crypto_secretbox_MACBYTES +
-      sod.crypto_secretbox_NONCEBYTES +
-      sod.crypto_pwhash_SALTBYTES)
-    const salt = cipher.subarray(0, sod.crypto_pwhash_SALTBYTES)
-    const nonce = cipher.subarray(sod.crypto_pwhash_SALTBYTES,
-      sod.crypto_pwhash_SALTBYTES + sod.crypto_secretbox_NONCEBYTES)
+      crypto_secretbox_MACBYTES +
+      crypto_secretbox_NONCEBYTES +
+      crypto_pwhash_SALTBYTES)
+    const salt = cipher.subarray(0, crypto_pwhash_SALTBYTES)
+    const nonce = cipher.subarray(crypto_pwhash_SALTBYTES,
+      crypto_pwhash_SALTBYTES + crypto_secretbox_NONCEBYTES)
     const pw = Buffer.allocUnsafe(difficulty)
-    sod.randombytes_buf(salt)
-    sod.randombytes_buf(nonce)
-    sod.randombytes_buf(pw)
+    randombytes_buf(salt)
+    randombytes_buf(nonce)
+    randombytes_buf(pw)
 
-    const secret = Buffer.allocUnsafe(sod.crypto_secretbox_KEYBYTES)
+    const secret = Buffer.allocUnsafe(crypto_secretbox_KEYBYTES)
 
-    sod.crypto_pwhash(secret, pw, salt,
-      sod.crypto_pwhash_OPSLIMIT_INTERACTIVE,
-      sod.crypto_pwhash_MEMLIMIT_INTERACTIVE,
-      sod.crypto_pwhash_ALG_DEFAULT)
+    crypto_pwhash(secret, pw, salt,
+      crypto_pwhash_OPSLIMIT_INTERACTIVE,
+      crypto_pwhash_MEMLIMIT_INTERACTIVE,
+      crypto_pwhash_ALG_DEFAULT)
 
-    sod.crypto_secretbox_easy(
-      cipher.subarray(sod.crypto_pwhash_SALTBYTES + sod.crypto_secretbox_NONCEBYTES),
+    crypto_secretbox_easy(
+      cipher.subarray(crypto_pwhash_SALTBYTES + crypto_secretbox_NONCEBYTES),
       data, nonce, secret)
 
     return cipher
@@ -197,35 +200,35 @@ const Util = {
     // If you want higher difficulty then go play with PoW instead.
     // (or open an issue if you believe that I messed up)
     if (difficulty > 3) throw new Error('Please mind the environment')
-    const salt = comb.subarray(0, sod.crypto_pwhash_SALTBYTES)
-    const cipher = comb.subarray(sod.crypto_pwhash_SALTBYTES)
+    const salt = comb.subarray(0, crypto_pwhash_SALTBYTES)
+    const cipher = comb.subarray(crypto_pwhash_SALTBYTES)
     const pw = Buffer.allocUnsafe(difficulty)
-    const nonceLen = sod.crypto_secretbox_NONCEBYTES
+    const nonceLen = crypto_secretbox_NONCEBYTES
     const nonce = cipher.subarray(0, nonceLen) // First part of the buffer
-    const headerSz = sod.crypto_secretbox_MACBYTES +
-      sod.crypto_secretbox_NONCEBYTES +
-      sod.crypto_pwhash_SALTBYTES
+    const headerSz = crypto_secretbox_MACBYTES +
+      crypto_secretbox_NONCEBYTES +
+      crypto_pwhash_SALTBYTES
 
     const messageLen = comb.length - headerSz
 
     const message = Buffer.allocUnsafe(messageLen)
-    const secret = Buffer.allocUnsafe(sod.crypto_secretbox_KEYBYTES)
+    const secret = Buffer.allocUnsafe(crypto_secretbox_KEYBYTES)
     let success = false
 
     const tried = []
     while (!success) { // TODO: maybe add timeout.
-      sod.randombytes_buf(pw)
+      randombytes_buf(pw)
       // This does not work for higher diff
       // but i'm leaving it as is for now
       if (tried[pw[0]]) continue
       else tried[pw[0]] = true
-      sod.crypto_pwhash(secret, _knownKey || pw, salt,
-        sod.crypto_pwhash_OPSLIMIT_INTERACTIVE,
-        sod.crypto_pwhash_MEMLIMIT_INTERACTIVE,
-        sod.crypto_pwhash_ALG_DEFAULT)
-      success = sod.crypto_secretbox_open_easy(
+      crypto_pwhash(secret, _knownKey || pw, salt,
+        crypto_pwhash_OPSLIMIT_INTERACTIVE,
+        crypto_pwhash_MEMLIMIT_INTERACTIVE,
+        crypto_pwhash_ALG_DEFAULT)
+      success = crypto_secretbox_open_easy(
         message,
-        comb.subarray(sod.crypto_pwhash_SALTBYTES + sod.crypto_secretbox_NONCEBYTES),
+        comb.subarray(crypto_pwhash_SALTBYTES + crypto_secretbox_NONCEBYTES),
         nonce,
         secret
       )
@@ -239,52 +242,53 @@ const Util = {
 
   // not supported in sodium-universal
   _box (to, from, m) {
-    const o = Buffer.allocUnsafe(sod.crypto_box_MACBYTES +
-      sod.crypto_box_NONCEBYTES +
+    const o = Buffer.allocUnsafe(crypto_box_MACBYTES +
+      crypto_box_NONCEBYTES +
       m.length)
-    const n = o.subarray(0, sod.crypto_box_NONCEBYTES)
-    sod.randombytes_buf(n)
-    const c = o.subarray(sod.crypto_box_NONCEBYTES)
-    sod.crypto_box_easy(c, m, n, to, from)
+    const n = o.subarray(0, crypto_box_NONCEBYTES)
+    randombytes_buf(n)
+    const c = o.subarray(crypto_box_NONCEBYTES)
+    crypto_box_easy(c, m, n, to, from)
     return o
   },
   // not supported in sodium-universal
   _unbox (from, to, c) {
     const m = Buffer.allocUnsafe(c.length -
-      sod.crypto_box_MACBYTES -
-      sod.crypto_box_NONCEBYTES)
-    const n = c.subarray(0, sod.crypto_box_NONCEBYTES)
-    const succ = sod.crypto_box_open_easy(m, c.subarray(sod.crypto_box_NONCEBYTES), n, from, to)
+      crypto_box_MACBYTES -
+      crypto_box_NONCEBYTES)
+    const n = c.subarray(0, crypto_box_NONCEBYTES)
+    const succ = crypto_box_open_easy(m, c.subarray(crypto_box_NONCEBYTES), n, from, to)
     if (!succ) throw new Error('DecryptionFailedError')
     return m
   },
   seal (m, pk) {
-    const c = Buffer.allocUnsafe(sod.crypto_box_SEALBYTES + m.length)
-    sod.crypto_box_seal(c, m, pk)
+    const c = Buffer.allocUnsafe(crypto_box_SEALBYTES + m.length)
+    crypto_box_seal(c, m, pk)
     return c
   },
   unseal (c, sk, pk) {
-    const m = Buffer.allocUnsafe(c.length - sod.crypto_box_SEALBYTES)
-    const succ = sod.crypto_box_seal_open(m, c, pk, sk)
+    const m = Buffer.allocUnsafe(c.length - crypto_box_SEALBYTES)
+    const succ = crypto_box_seal_open(m, c, pk, sk)
     if (!succ) throw new Error('DecryptionFailedError')
     return m
   }
 }
+
 module.exports = Util
 
 module.exports.Identity = class Identity {
   constructor (keys = {}) {
     this.box = {
-      pub: keys.bpk || Buffer.allocUnsafe(sod.crypto_box_PUBLICKEYBYTES),
-      sec: keys.bsk || Buffer.allocUnsafe(sod.crypto_box_SECRETKEYBYTES)
+      pub: keys.bpk || Buffer.allocUnsafe(crypto_box_PUBLICKEYBYTES),
+      sec: keys.bsk || Buffer.allocUnsafe(crypto_box_SECRETKEYBYTES)
     }
-    if (!keys.bsk) sod.crypto_box_keypair(this.box.pub, this.box.sec)
+    if (!keys.bsk) crypto_box_keypair(this.box.pub, this.box.sec)
 
     this.sig = {
-      pub: keys.spk || Buffer.allocUnsafe(sod.crypto_sign_PUBLICKEYBYTES),
-      sec: keys.ssk || Buffer.allocUnsafe(sod.crypto_sign_SECRETKEYBYTES)
+      pub: keys.spk || Buffer.allocUnsafe(crypto_sign_PUBLICKEYBYTES),
+      sec: keys.ssk || Buffer.allocUnsafe(crypto_sign_SECRETKEYBYTES)
     }
-    if (!keys.ssk) sod.crypto_sign_keypair(this.sig.pub, this.sig.sec)
+    if (!keys.ssk) crypto_sign_keypair(this.sig.pub, this.sig.sec)
   }
 
   static encode (id, b, o) {
@@ -313,8 +317,8 @@ module.exports.DerivedIdentity = class DerivedIdentity extends Identity {
     super()
     this.master = mk
     if (!this.master) {
-      this.master = Buffer.alloc(sod.crypto_kdf_KEYBYTES)
-      sod.crypto_kdf_keygen(this.master)
+      this.master = Buffer.alloc(crypto_kdf_KEYBYTES)
+      crypto_kdf_keygen(this.master)
     }
     // Signing keys
     this.sig = Util.deriveSignPair(this.master)
@@ -361,8 +365,8 @@ module.exports.Poll = class Poll extends PicoFeed {
 
   packVote (ident, vote, append = false) {
     if (!Buffer.isBuffer(vote)) throw new Error('vote must be a buffer')
-    const b0 = Buffer.alloc(sod.crypto_secretbox_KEYBYTES)
-    sod.randombytes_buf(b0)
+    const b0 = Buffer.alloc(crypto_secretbox_KEYBYTES)
+    randombytes_buf(b0)
     const msg = {
       ballot: {
         // This is also annoying, the bpk was supposed to be published
@@ -420,9 +424,9 @@ module.exports.Poll = class Poll extends PicoFeed {
 // Export for external use.
 module.exports.Poll.Statement = PollStatement
 module.exports.VoteMsg = VoteMsg
-module.exports.Buffer = Buffer
-module.exports.sodium = sod
-
+// module.exports.Buffer = Buffer
+/*
 if (typeof window !== 'undefined') {
   window.Cryptology = module.exports
 }
+*/
